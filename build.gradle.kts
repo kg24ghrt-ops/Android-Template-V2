8258e95c643b2b45bf40b6f7455287c4d32a6bad
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-03-22 08:30:08 +0630

    FIX: Forcing SweetDependency autowire before build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    autowire(libs.plugins.android.application) apply false
-    autowire(libs.plugins.android.library) apply false
-    autowire(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
