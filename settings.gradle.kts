9a25484ab3aa6f8a4f532ca5ba72a9cd67a0d584
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-03-22 07:59:10 +0630

    FIX: Forcing SweetDependency autowire before build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,21 @@ pluginManagement {
         mavenCentral()
     }
 }
+
 plugins {
     id("com.highcapable.sweetdependency") version "1.0.4"
     id("com.highcapable.sweetproperty") version "1.0.8"
 }
+
 sweetProperty {
-    rootProject { all { isEnable = false } }
+    // Disable automation for all subprojects by default
+    rootProject { 
+        all { 
+            isEnable = false 
+        } 
+    }
 }
+
+// Project name and modules
 rootProject.name = "movie"
 include(":app")
